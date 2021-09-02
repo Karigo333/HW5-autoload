a240a0e4f84748ed0801ae863ba8b8ba9108db68
Igor <44480143+Karigo333@users.noreply.github.com>
2021-09-02 10:36:41 +0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -12,32 +12,39 @@ class Autoloader
 
     public function addNamespaces($prefix, $base_dir){
 
-        if(preg_match('/[\/#$%|\*]/sui', $prefix) > 0) {
+        if(preg_match('/[\/#$%|\*]/sui', $prefix) > 0) 
+        {
             throw new Exception('Bad namespace prefix');
         }
-        elseif (preg_match('/[\\#$%|*]/sui', $base_dir) > 0) {
+        elseif (preg_match('/[\\#$%|*]/sui', $base_dir) > 0) 
+        {
             throw new Exception('Bad base_dir path');
         }
 
-        if (!isset($this->namespaces[$prefix])) {
+        if (!isset($this->namespaces[$prefix])) 
+        {
             $this->namespaces[$prefix] = array();
         }
-        if (substr($base_dir, -1) != '/') {
+        if (substr($base_dir, -1) != '/') 
+        {
             $base_dir .= '/';
         }
 
         array_push($this->namespaces[$prefix], $base_dir);
     }
 
-    public function getClasses($class) {
+    public function getClasses($class) 
+    {
         $class_explode = explode('\\', $class);
         $filename = array_pop($class_explode);
         $namespace = implode('\\', $class_explode);
 
-        foreach ($this->namespaces[$namespace] as $key => $basedir) {
+        foreach ($this->namespaces[$namespace] as $key => $basedir)
+        {
             $file = $basedir . $filename . ".php";
 
-            if (file_exists(main_dir.$file)) {
+            if (file_exists(main_dir.$file))
+            {
                 require main_dir . $file;
                 return true;
             }
