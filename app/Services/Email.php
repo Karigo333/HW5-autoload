ae35b9628e059c339f6a30a3c8d2fc88e1a68027
Igor <44480143+Karigo333@users.noreply.github.com>
2021-09-02 10:35:42 +0300

    Update Email.php

--- app/Services/Email.php
+++ app/Services/Email.php
@@ -10,7 +10,8 @@ class Email
 
     public function __construct($value)
     {
-        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) 
+        {
             throw new Exception('invalid mail');
         }
 
