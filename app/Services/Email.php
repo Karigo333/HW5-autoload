22f6452137c357a7f752b37d2cef53f9c18424d0
Igor <44480143+Karigo333@users.noreply.github.com>
2021-09-02 10:13:26 +0300

    Update Email.php
    clear code

--- app/Services/Email.php
+++ app/Services/Email.php
@@ -17,24 +17,3 @@ class Email
         $this->value = $value;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
-
-    public function doLogic()
-    {
-        $a = 8;
-        $b = 10;
-
-        $c = $this->sum($a, $b);
-    }
-
-    protected function sum($a, $b)
-    {
-        return $a + $b;
-    }
-}
